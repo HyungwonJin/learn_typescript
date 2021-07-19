c41469c68aeacd9ec33da6771f20454a13a34ca9
HyungwonJin <86827200+HyungwonJin@users.noreply.github.com>
2021-07-19 13:47:11 +0900

    How to work Interface in Typescript

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-console.log("hello");
+const person = {
+    name: "hy",
+    age: 20,
+    gender: "male"
+};
+const sayHI = (person) => {
+    return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
+};
+console.log(sayHI(person));
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,1 +1,17 @@
-console.log("hello");
+interface Human {
+    name: string;
+    age: number;
+    gender: string;
+}
+
+const person = {
+    name: "hy",
+    age: 20,
+    gender: "male"
+}
+
+const sayHI = (person: Human): string => {
+    return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`
+}
+
+console.log(sayHI(person));
