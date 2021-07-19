3c6f799004c9464efd65963cfc403622720bd440
HyungwonJin <86827200+HyungwonJin@users.noreply.github.com>
2021-07-19 13:58:55 +0900

    How to work class in Typescript

--- index.ts
+++ index.ts
@@ -1,17 +1,30 @@
-interface Human {
-    name: string;
-    age: number;
-    gender: string;
+// interface Human {
+//     name: string;
+//     age: number;
+//     gender: string;
+// }
+
+// const person = {
+//     name: "hy",
+//     age: 20,
+//     gender: "male"
+// }
+
+class Human {
+    public name: string;
+    public age: number;
+    public gender: string;
+    constructor(name: string, age: number, gender: string){
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
 }
 
-const person = {
-    name: "hy",
-    age: 20,
-    gender: "male"
-}
+const hyung = new Human("hyung", 20, "male");
 
 const sayHI = (person: Human): string => {
     return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`
 }
 
-console.log(sayHI(person));
+console.log(sayHI(hyung));
